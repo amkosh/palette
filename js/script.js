06c952f03bf9d2fc6ede7fa9f40fddaada573a7b
amkosh <amkosh@mail.ru>
2022-05-05 19:16:11 +0500

    +rebuild

--- js/script.js
+++ js/script.js
@@ -1,56 +1,128 @@
-let cont = document.getElementById('container');
+//Контейнер для срабатывания нажатий и блоки RGB + результат
+let container = document.getElementById('container');
 let result = document.getElementById('result');
-let r = document.getElementById('red');
-let g = document.getElementById('green');
-let b = document.getElementById('blue');
+let canvas = document.getElementById('drawning');
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
 
-//Добавляем считывание действий
-cont.addEventListener('mousedown', changeColor);
-result.addEventListener('click', changeResColor);
+//Listener нажатия/отжатия кнопок
+container.addEventListener('mousedown', trigger);
+container.addEventListener('mouseup', trigger);
+result.addEventListener('click', randomColor);
 
+//Состояния (нажатие, +/-, текущий блок)
+let isPress = false;
+let sign = true;
+let current = null;
 
-//Получаем нужный цвет в RGB формате с учетом блока
-function getColor (color, value) {
-    switch(color) {
-        case 'red': return 'rgb(' + value + ',0,0)';
+//Счетчик итераций
+let iteration = 1;
+
+//Переключение состояний нажатий мыши
+function trigger (event) {
+    if(event.which == 1) {
+        current = event.target;
+        isPress = !isPress;
+        sign = true;
+    } else if (event.which == 3) {
+        current = event.target;
+        isPress = !isPress;
+        sign = false;
+    }
+    iteration = 1;
+}
+
+//Интервальное обновление
+setInterval(function func() {
+        //Проверка выбора блока
+        if(current){
+            //Множитель
+            let mult = 1;
+
+            //Проверка кол-ва итераций, увеличение множителя
+            if(iteration > 10){
+                mult = 11;
+            }
+
+            //Увеличение значений с учетом множителя
+            if(isPress && sign){
+                current.innerText = Number(current.innerText) + (1 * mult);
+                
+                if(current.innerText > 255){
+                    current.innerText = 0;
+                }
+
+                colorChange();
+                document.getElementById('hex').innerText = rgb2hex(result.style.background).toUpperCase();
+
+            } else if (isPress && !sign){
+                current.innerText = Number(current.innerText) - (1 * mult);
+
+                if (current.innerText < 0){
+                    current.innerText = 255;
+                }
+                
+                colorChange();
+                document.getElementById('hex').innerText = rgb2hex(result.style.background).toUpperCase();
+            }
+
+            //Счетчик итераций
+            iteration++;
+        }
+    }, 100
+);
+
+//Определение цвета блока
+function colorChange(){
+    switch (current.id){
+        case 'red': current.style.background = 'rgb(' + current.innerText + ',0,0)';
         break;
-        case 'green': return 'rgb(0,' + value + ',0)';
+        case 'green': current.style.background = 'rgb(0,' + current.innerText + ',0)';
         break;
-        case 'blue': return 'rgb(0,0,' + value + ')';
+        case 'blue': current.style.background = 'rgb(0,0,' + current.innerText + ')';
         break;
     }
+
+    //Обновление блока смешивания цветов
+    result.style.background = 'rgb(' + red.innerText + ',' + green.innerText + ',' + blue.innerText + ')';
 }
 
-//Меняем цвет, увеличиваем значение счетчика
-function changeColor (event) {
-    if(event.target.innerText >= 255){
-        event.target.innerText = 0;
-    } else {
-        event.target.innerText++;
+//Генерация произвольного цвета и обновление блоков
+function randomColor () {
+    red.innerText = Math.ceil(Math.random()*255);
+    red.style.background = 'rgb(' + red.innerText + ',0,0)';
+    green.innerText = Math.ceil(Math.random()*255);
+    green.style.background = 'rgb(0,' + green.innerText + ',0)';
+    blue.innerText = Math.ceil(Math.random()*255);
+    blue.style.background = 'rgb(0,0,' + blue.innerText + ')';
+    result.style.background = 'rgb(' + red.innerText + ',' + green.innerText + ',' + blue.innerText + ')';
+    document.getElementById('hex').innerText = rgb2hex(result.style.background).toUpperCase();
+}
+
+//Запрет срабатывания контекстного меню
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    }, false);
+} else {
+    document.attachEvent('oncontextmenu', function () {
+        window.event.returnValue = false;
+    });
+}
+
+//Перевод в 16-е значение
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
     }
-    let color = getColor(event.target.id, event.target.innerText);
-    event.target.style.background = color;
-    resultUpdate();
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
-//Обновляем цвет блока с результатом смешивания
-function resultUpdate(){
-    result.style.background = 'rgb(' + r.innerText + ',' + g.innerText + ',' + b.innerText +')';
-}
-
-//Генерируем цвет блока с результатом
-function changeResColor () {
-    let red = Math.ceil(Math.random() * 255);
-    let green = Math.ceil(Math.random() * 255);
-    let blue = Math.ceil(Math.random() * 255);
-    let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
-    result.style.background = color;
-
-    //Обновляем значения блоков в соответстви со сгенерированным цветом
-    r.innerText = red;
-    r.style.background = 'rgb(' + red + ',0,0)';
-    g.innerText = green;
-    g.style.background = 'rgb(0,' + green + ',0)';
-    b.innerText = blue;
-    b.style.background = 'rgb(0,0,' + blue + ')';
+//Рисование
+function draw() {
+    let dot = document.createElement('p');
+    dot.style.background = result.style.background;
+    canvas.appendChild(dot);
 }
